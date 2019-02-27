900cff4a42782912e688ec2980eeb4426db0c9a3
yq501658204 <501658204@qq.com>
2019-02-27 17:07:47 +0800

    新增上传图片功能

--- src/main/java/com/example/Hello/controller/AppController.java
+++ src/main/java/com/example/Hello/controller/AppController.java
@@ -1,13 +1,16 @@
 package com.example.Hello.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.Hello.util.FileUtil;
 import net.sf.json.JSONObject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/app")
@@ -17,108 +20,15 @@ public class AppController {
 	public void getTime(String timenow){
 		System.out.println("看看得到的时间是什么样子的："+timenow);
 	}
-	
-	@RequestMapping("/getUser")
+
+	@RequestMapping("/upload")
 	@ResponseBody
-	public JSONObject getUser(){
-		JSONObject result = new JSONObject();
-		result.put("id", 132);
-		result.put("username", 132);
-		result.put("sex", 1);
-		result.put("city", 132);
-		result.put("sign", 132);
-		result.put("experience", 132);
-		result.put("score", 132);
-		result.put("classify", 132);
-		result.put("wealth", 132);
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("code", 0);
-		jsonObject.put("count", 1);
-		jsonObject.put("msg", "");
-		List<JSONObject> list=new ArrayList<JSONObject>();
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		list.add(result);
-		jsonObject.put("data", list);
-		return jsonObject;
+	public String upload(MultipartFile file) throws Exception {
+		byte[] bytes = file.getBytes();
+		String originalFilename = file.getOriginalFilename();
+		FileUtil.uploadFile(bytes,"F:\\YQ\\Project\\Hello\\src\\main\\resources\\static\\img\\",originalFilename);
+		return "OJBK";
 	}
-	
+
+
 }
